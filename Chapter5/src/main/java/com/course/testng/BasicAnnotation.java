c5ca98dee733ac7454ac250f6b52444316ad963f
tyler <m_chenjiafeng@163.com>
2019-10-11 14:39:07 +0800

    testNG的使用

---
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,40 @@
+package com.course.testng;
+
+import org.testng.annotations.*;
+
+/**
+ * @author tyler.chen
+ * @data 2019-10-11 13:39
+ */
+public class BasicAnnotation {
+    @Test
+    public void testcase1(){
+        System.out.println("这是测试用例1");
+
+    }
+    @Test
+    public void testcase2(){
+        System.out.println("这是测试用例2");
+    }
+
+    @BeforeMethod
+    public  void beforeMethod(){
+        System.out.println("BeforeMethod这是在测试方法之前运行的");
+
+    }
+    @AfterMethod
+    public void afterMethond(){
+        System.out.println("AfterMethod这是在测试方法之后运行的");
+
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("BeforeClass这是在类运行之前运行的");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("AfterClass这是子类运行之后运行的");
+
+    }
+}
